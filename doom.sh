6fda83562eee6b36118561a331510cfcc046657e
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2014-12-28 14:10:33 +0100

    Remowed test.wav and added default value to the RMS amplitude

--- doom.sh
+++ doom.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 echo "Hold [CTRL+C] to stop.."
 while true; do
+  threshold=$1
+  if [ -z "$threshold" ]
+  then
+     threshold=0.02
+  fi
   printf "Listening...\r"
   sleep 1
   ./sox/rec -q ./test.wav trim 0 00:02
   value=`(./sox/sox -t .wav ./test.wav -n stat 2>&1 | grep -e "RMS.*amplitude" | tr -d ' ' | cut -d ':' -f 2)`
-  threshold=$1
   #echo $value
   #echo $threshold
   c=$(echo "$value > $threshold " | bc)
